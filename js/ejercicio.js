b93cf0686359f438bfcbd5a087cf5e33e4dd4d5b
Nicolas Gabriel Pradines <83048964+NicoNetLog2021@users.noreply.github.com>
2023-03-07 19:48:39 -0300

    Add files via upload

---
+++ js/ejercicio.js
@@ -0,0 +1,12 @@
+
+function datos(){
+    
+    document.getElementById('telefono').innerHTML='123456789';
+    document.getElementById('loca').innerHTML='Vicente Lopez';
+    document.getElementById('ema').innerHTML='nicolas.pradines@hotmail.com';
+}
+
+function enviar(){
+
+    alert("Mensaje enviado");
+}
